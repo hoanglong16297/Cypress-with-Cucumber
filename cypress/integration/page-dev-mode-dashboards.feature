66df7e04c004914e1e77601bd522804a41453ade
LoiDoEoh <do.loi@eoh.io>
2023-06-08 17:47:35 +0700

    add case delete a unit on devmode dashboard page

--- cypress/integration/page-dev-mode-dashboards.feature
+++ cypress/integration/page-dev-mode-dashboards.feature
@@ -12,8 +12,24 @@ Feature: Page Devmode Dashboards
     Then I see display the expected greater than 1 element in table
 
   Scenario: Creating a unit on the Devmode Dashboards page
-    When should allow the browser to know your location
+    When I should allow the browser to know your location "10.8231" "106.6297"
     And I click button create unit at page devmode dashboard
-    And I enter "Unit-test-e2e" into the unit name field
-    And I click button submit create unit
+    And I enter "Unit-test-e2e-at-devmode-dashboards" into the unit name field
+    And I click the submit button on the modal page of Devmode Dashboard
     Then I should see the success message "Unit added successfully"
+
+  Scenario: Editing a unit on the Devmode Dashboards page
+    When I type "Unit-test-e2e-at-devmode-dashboards" in the search input
+    When I click dropdown at page devmode dashboard
+    When I click edit unit detail at page devmode dashboard
+    When I enter "Unit-test-e2e-rename-at-devmode-dashboards" into the unit name field
+    And  I click the submit button on the modal page of Devmode Dashboard
+    Then I should see the success message "Unit updated successfully"
+
+  Scenario: Deleting a unit on the Devmode Dashboards page
+    When I type "Unit-test-e2e-rename-at-devmode-dashboards" in the search input
+    When I click dropdown at page devmode dashboard
+    When I click delete unit detail at page devmode dashboard
+    When I click checkbox understand that this action
+    When I click the submit button on the modal page of Devmode Dashboard
+    Then I should see the success message "Unit deleted successfully"
--- cypress/integration/step_definitions/page-dev-mode-dashboards.js
+++ cypress/integration/step_definitions/page-dev-mode-dashboards.js
@@ -11,30 +11,50 @@ Then('I see display the expected greater than 1 element in table', () => {
   );
 });
 
-When('should allow the browser to know your location', () => {
-  cy.window().then((win) => {
-    cy.stub(win.navigator.geolocation, 'getCurrentPosition').callsFake(
-      (callback) => {
-        const position = {
-          coords: {
-            latitude: 10.8231, // Latitude for Ho Chi Minh, Vietnam
-            longitude: 106.6297, // Longitude for Ho Chi Minh, Vietnam
-          },
-        };
-        callback(position);
-      }
-    );
-  });
-});
+When(
+  'I should allow the browser to know your location {string} {string}',
+  (latitude, longitude) => {
+    cy.window().then((win) => {
+      cy.stub(win.navigator.geolocation, 'getCurrentPosition').callsFake(
+        (callback) => {
+          const position = {
+            coords: {
+              latitude: Number(latitude), // Latitude for Ho Chi Minh, Vietnam
+              longitude: Number(longitude), // Longitude for Ho Chi Minh, Vietnam
+            },
+          };
+          callback(position);
+        }
+      );
+    });
+  }
+);
 
 When('I click button create unit at page devmode dashboard', () => {
   cy.get('[testid="BUTTON_HEADER"]').click();
 });
 
 When('I enter {string} into the unit name field', (phoneNumber) => {
+  cy.get('[testid="INPUT_NAME_UNIT"]').clear();
   cy.get('[testid="INPUT_NAME_UNIT"]').type(phoneNumber);
 });
 
-When('I click button submit create unit', () => {
+When('I click the submit button on the modal page of Devmode Dashboard', () => {
   cy.get('[testid="BUTTON_SUBMIT"]').click();
+});
+
+When('I click dropdown at page devmode dashboard', () => {
+  cy.get('[testid="WRAP_DROPDOWN"]').click();
+});
+
+When('I click edit unit detail at page devmode dashboard', () => {
+  cy.get('[id="idEditUnit"]').click();
+});
+
+When('I click delete unit detail at page devmode dashboard', () => {
+  cy.get('[id="idDeleteUnit"]').click();
+});
+
+When('I click checkbox understand that this action', () => {
+  cy.get('[id="checkSquare"]').click();
 });
