b7e78784abec753aa0bbf29a71433035729681cf
LoiDoEoh <do.loi@eoh.io>
2023-06-08 18:00:44 +0700

    check husky

--- cypress/integration/step_definitions/language.js
+++ cypress/integration/step_definitions/language.js
@@ -4,7 +4,7 @@ When('I click the language setting icon', () => {
   cy.get('[testid="WRAP_ICON_LANGUAGE"]').click();
 });
 
-Then('I see language items', (dataTable) => {
+  Then('I see language items', (dataTable) => {
   dataTable.rawTable.forEach((row) => {
     const language = row[0].trim();
     cy.contains(language).should('be.visible');
