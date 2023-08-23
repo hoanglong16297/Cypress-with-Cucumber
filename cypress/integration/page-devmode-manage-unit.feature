dbc3c3ab918af6d715b809c5877016d3451e12fa
hoanglong160297 <hoanglong160297@gmail.com>
2023-08-23 17:30:08 +0700

    automation test page devmode manage unit

--- cypress/integration/page-devmode-manage-unit.feature
+++ cypress/integration/page-devmode-manage-unit.feature
@@ -2,7 +2,7 @@ Feature: Page Dashboard Manage Unit
 
   Background:
     Given I visit the login page
-    When I login with 0359579079 and Long1602
+    When I login with 0987655555 and f:Q~1x%ga'r)+=a
     And I check the sectionTab all dashboard
 
   Scenario: Create Unit
@@ -10,24 +10,30 @@ Feature: Page Dashboard Manage Unit
     And  I click the Add Unit button
     And  I click button location
     And  I provide a name for the unit
-    And  I click Creat Button
+    And  I click Button Submit
     Then I should see the success message "Unit added successfully"
 
-  Scenario: Edit-Add units
+  Scenario: Edit-Add-Delete units and station
     When I visit Manage Unit at page devmode dashboard
-    And  I search for "Long-test-devmode-unit" in the search input
+    And  I search for "Unit-test-devmode-unit" in the search input
     And  I Edit Unit
-    And  I search for "Long-test-edit-unit" in the search input
+    And  I search for "Unit-test-edit-unit" in the search input
     And  I Add a Sub Unit
+    And  I Edit Sub Unit
+    And  I Delete Sub Unit
+    And  I Delete Unit
     Then I check the sectionTab all dashboard
   
   Scenario: Edit-Add-Delete devices
     When I visit Manage Unit at page devmode dashboard
-    And  I search for "Long-test-edit-unit" in the search input
+    And  I search for "Unit-test-e2e-not-remove" in the search input
     And  I Edit Sub Unit
     And  I Add a New Device Display
-  #   And  I Delete Sub Unit
-  #   Then I should see the success message "Sub unit added successfully"
+    And  I provide a name for the display device
+    And  I change icon
+    And  I select device
+    And  I Delete End Device
+    Then I should see the success message "Delete successful"
 
 
     
--- cypress/integration/step_definitions/page-devmode-manage-unit.js
+++ cypress/integration/step_definitions/page-devmode-manage-unit.js
@@ -1,8 +1,7 @@
 import {When} from 'cypress-cucumber-preprocessor/steps';
-import Routes from '../../../utils/Route';
 
 When('I visit Manage Unit at page devmode dashboard', () => {
-  cy.get('[id="BUTTON_MANAGE_UNIT"]').click();
+  cy.get('[testid="BUTTON_MANAGE_UNIT"]').click();
 });
 
 When('I click the Add Unit button', () => {
@@ -15,101 +14,84 @@ When('I click button location', () => {
 
 When('I provide a name for the unit', () => {
   cy.get('[testid="INPUT_NAME_UNIT"]')
-    .type('Long-test-devmode-unit')
-    .should('have.value', 'Long-test-devmode-unit');
+    .type('Unit-test-devmode-unit').should('have.value', 'Unit-test-devmode-unit');
 });
 
 When('I search for {string} in the search input', (searchText) => {
-  cy.get('[testid="SEARCH_INPUT"]')
-    .clear()
-    .type(searchText);
+  cy.get('[testid="SEARCH_INPUT"]').clear({ force: true }).type(searchText, { force: true });
 });
 
-When('I click Creat Button', () => {
+When('I click Button Submit', () => {
   cy.get('[testid="BUTTON_SUBMIT"]').click();
 });
 
-When('I scroll to top page', () => {
-  cy.get('SEARCH_INPUT')
-  .shadow()
-  .find('[data-test-id="my-button"]')
-  .click({ position: 'top' })
-});
-
-
 When('I Edit Unit', () => {
-  cy.get('[testid="EXPAND_UNIT_DROPDOWN_UNIT"]')
-    .eq(0)
-    .click();
-  cy.get('[id="idEditUnit"]')
-    .click();
-  cy.get('[testid="INPUT_NAME_UNIT"]')
-    .clear()
-    .type('Long-test-edit-unit')
-    .should('have.value', 'Long-test-edit-unit');
-  cy.get('[testid="BUTTON_SUBMIT"]')
-    .click();
+  cy.get('[testid="EXPAND_UNIT_DROPDOWN_UNIT"]').eq(0).click();
+  cy.get('[testid="MODAL_UNIT_EDIT_UNIT"]').click();
+  cy.get('[testid="INPUT_NAME_UNIT"]').clear()
+    .type('Unit-test-edit-unit').should('have.value', 'Unit-test-edit-unit');
+  cy.get('[testid="BUTTON_SUBMIT"]').click();
 });
 
 When('I Add a Sub Unit', () => {
-  cy.get('[testid="EXPAND_UNIT_DROPDOWN_UNIT"]')
-    .eq(0)
-    .click();
-  cy.get('[id="idAddSubUnit"]')
-    .click();
-  cy.get('[class="ant-input css-dev-only-do-not-override-bbd4ux"]')
-    .eq(2)
-    .type('Long-test-add-unit')
-    .should('have.value', 'Long-test-add-unit'); 
+  cy.get('[testid="EXPAND_UNIT_DROPDOWN_UNIT"]').eq(0).click();
+  cy.get('[testid="MODAL_UNIT_ADD_SUB_UNIT"]').click();
+  cy.get('[testid="MODAL_INPUT_CHANGE_SUB_UNIT_NAME"]')
+    .type('Unit-test-add-unit').should('have.value', 'Unit-test-add-unit'); 
   cy.get('[testid="BUTTON_SUBMIT"]').eq(1).click();
 });
 
 When('I Delete Unit', () => {
-  cy.get('[testid="EXPAND_UNIT_DROPDOWN_UNIT"]')
-    .eq(0)
-    .click();
-  cy.get('[id="idDeleteUnit]')
-    .click();
+  cy.get('[testid="EXPAND_UNIT_DROPDOWN_UNIT"]').eq(0).click();
+  cy.get('[testid="MODAL_UNIT_DELETE_UNIT"]').click();
   cy.get('[id="checkSquare"]').click();
   cy.get('[id="buttonDelete"]').click();
 });
 
 When('I Edit Sub Unit', () => {
-  cy.get('[testid="EXPAND_UNIT_DROPDOWN_SUB_UNIT"]')
-    .eq(0)
-    .click();
-  cy.get('[id="idEditSubUnit"]').click();
-  cy.get('[class="ant-input css-dev-only-do-not-override-bbd4ux"]')
-    .eq(1)
-    .clear()
-    .type('Long-test-edit-sub-unit')
-    .should('have.value', 'Long-test-edit-sub-unit');
+  cy.get('[testid="EXPAND_UNIT_DROPDOWN_SUB_UNIT"]').eq(0).click();
+  cy.get('[testid="MODAL_SUB_UNIT_EDIT_SUB_UNIT"]').click();
+  cy.get('[testid="MODAL_INPUT_CHANGE_SUB_UNIT_NAME"]').clear()
+    .type('e2e-add-new-subunit').should('have.value', 'e2e-add-new-subunit');
   cy.get('[id="buttonSubmit"]').click()
 });
 
 When('I Add a New Device Display', () => {
-  cy.get('[testid="EXPAND_UNIT_DROPDOWN_SUB_UNIT"]').click();
-  cy.get('[id="idAddNewDeviceDisplay"]').click();
-  cy.get('[class="ant-input css-dev-only-do-not-override-bbd4ux"]')
-    .eq(2)
-    .type('Long-test-add-new-device-display')
-    .should('have.value', 'Long-test-add-new-device-display');
-  cy.get('[testid="BUTTON_COMMON"]')
-    .eq(1)
-    .click()
-  cy.get('[class="ant-input css-dev-only-do-not-override-bbd4ux"]')
-    .eq(3)
-    .type('123')
-    .should('have.value', '123');
-  cy.get('[testid="ICON"]')
-    .eq(10)
-    .click()
+  cy.get('[testid="EXPAND_UNIT_DROPDOWN_SUB_UNIT"]').eq(0 ).click();
+  cy.get('[testid="MODAL_SUB_UNIT_ADD_NEW_DEVICE"]').click();
+});
+
+When('I provide a name for the display device', () => {
+  cy.get('[testid="MODAL_INPUT_NAME_DISPLAY_DEVICE"]')
+    .type('Unit-test-add-new-device-display').should('have.value', 'Unit-test-add-new-device-display');
+
+});
+
+When('I change icon', () => {
+  cy.get('[testid="BUTTON_UPLOAD_IMAGE_UNIT"]').click()
+  cy.get('[testid="INPUT_ICON"]')
+    .type('123').should('have.value', '123');
+  cy.get('[testid="ICON"]').eq(10).click();
+});
+
+When('I select device', () => {
+  cy.get('[testid="SELECT_ROW_ITEM_MODAL_CHIP"]').eq(0).click();
+    cy.get('[class="ant-select-item ant-select-item-option ant-select-item-option-active"]').eq(0).click();
+  cy.get('[testid="SELECT_ROW_ITEM_MODAL_CHIP"]').eq(1).click();
+    cy.get('[class="ant-select-item ant-select-item-option ant-select-item-option-active"]').eq(1).click();  
+  cy.get('[id="submitCreate"]').click();
+});
+
+When('I Delete End Device', () => {
+  cy.get('[class="ant-col ant-col-xs-12 ant-col-sm-8 ant-col-md-6 ant-col-lg-4 css-dev-only-do-not-override-bbd4ux"]').eq(0).click();
+    cy.get('[testid="EXPAND_SUB_UNIT_DROPDOWN_END_DEVICE"]').click();
+    cy.get('[testid="MODAL_DELETE_END_DEVICE"]').click();
+    cy.get('[testid="BUTTON_SUBMIT"]').click();
+
 });
 
 When('I Delete Sub Unit', () => {
-  cy.get('[class="anticon anticon-ellipsis"]').eq(1).click();
-  cy.get('[class="ant-dropdown-menu-title-content"]')
-    .eq(5)
-    .click();
-  cy.get('[testid="BUTTON_SUBMIT"]').eq(1).click();
+  cy.get('[testid="EXPAND_UNIT_DROPDOWN_SUB_UNIT"]').eq(0).click();
+    cy.get('[testid="MODAL_SUB_UNIT_DELETE_SUB_UNIT"]').click();
+    cy.get('[id="submitDelete"]').click();
 });
